9218ecc761dc01c7932aa9a090c91010e19a99b5
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-14 17:13:28 -0500

    I am a magician and am improving all the time 8D

--- hw/takeOne/server.js
+++ hw/takeOne/server.js
@@ -10,10 +10,10 @@ app.get('/:numberOfBottles', (req, res) => {
     
     const totalBottles = req.params.numberOfBottles
 
-    if (totalBottles === 0) {
-        res.send(`<h1> 0 Bottles of beer on the wall</h1> <h2><a href="/">Start Over</a></h2>`)
+    if (totalBottles > 0) {
+        res.send(`<h1>${totalBottles} Bottles of beer on the wall</h1> <h2><a href="/${(totalBottles) - 1}">Take one down, pass it around</a></h2>`)
     } else {
-        res.send(`<h1>${totalBottles} Bottles of beer on the wall</h1> <h2><a href="/${Number(totalBottles) - 1}">Take one down, pass it around</a></h2>`)
+        res.send(`<h1> 0 Bottles of beer on the wall</h1> <h2><a href="/">Start Over</a></h2>`)
     }
 
 })
@@ -32,6 +32,8 @@ app.get('/:numberOfBottles', (req, res) => {
 //       res.send(`<a href="/">Start over</a>`)
 //     }
 //   }); not generating the hyperlink for the numbers passed 98, BUT WHY?
+
+// looking at the first code i tried, i see that the h1 and h2 had to be in the res.send together and that i didn't need Number, because that bit wasn't necessarily dependent on user input
 
 
 
