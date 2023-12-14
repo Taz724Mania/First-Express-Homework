49a68309c4339e25e7726840bf74a826db3bf404
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-14 17:02:05 -0500

    got it to function with the rest of them, but Start Over isn't showing up

--- hw/takeOne/server.js
+++ hw/takeOne/server.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>99 Bottles of beer on the wall</h1><h2><a href="/98">Take one down, pass it around</a></h2>')
+// app.get('/', (req, res) => {
+//     res.send('<h1>99 Bottles of beer on the wall</h1><h2><a href="/98">Take one down, pass it around</a></h2>')
+// })
+
+
+app.get('/:numberOfBottles', (req, res) => {
+    
+    const totalBottles = req.params.numberOfBottles
+
+    if (totalBottles === 0) {
+        res.send(`<h1> 0 Bottles of beer on the wall</h1> <h2><a href="/">Start Over</a></h2>`)
+    } else {
+        res.send(`<h1>${totalBottles} Bottles of beer on the wall</h1> <h2><a href="/${Number(totalBottles) - 1}">Take one down, pass it around</a></h2>`)
+    }
+
 })
 
-app.get('/:number_of_bottles', (req, res) => {
-    const bottles = req.params.number_of_bottles;
+
+
+
+// app.get('/:number_of_bottles', (req, res) => {
+//     const bottles = req.params.number_of_bottles
     
-    res.send(`<h1>${bottles} Bottles of beer on the wall</h1>`);
+//     res.send(`<h1>${bottles} Bottles of beer on the wall</h1>`)
   
-    if (bottles > 0) {
-      res.send(`<a href="/${Number(bottles) - 1}">Take one down, pass it around</a>`);
-    } else {
-      res.send(`<a href="/">Start over</a>`);
-    }
-  });
+//     if (bottles > 0) {
+//       res.send(`<h2><a href="/${Number(bottles) - 1}">Take one down, pass it around</a></h2>`)
+//     } else {
+//       res.send(`<a href="/">Start over</a>`)
+//     }
+//   }); not generating the hyperlink for the numbers passed 98, BUT WHY?
 
 
 
