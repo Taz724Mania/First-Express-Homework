51b1edaf2c6b478f171bb4f8c35452cb5c5a2690
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-13 23:55:42 -0500

    i told yinz i can't do math. had to google how to calculate a percentage *facepalm*

--- hw/tipCalc/server.js
+++ hw/tipCalc/server.js
@@ -4,3 +4,10 @@ app.listen(4000, () => {
     console.log('Hello, Sweetie')
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = req.params.total
+    const tipPercentage = req.params.tipPercentage
+    const tipAmount = total * (tipPercentage / 100)
+    res.send(`<h1>${tipAmount}</h1>`)
+    
+})
