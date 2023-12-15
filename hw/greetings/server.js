8986224f73b06761cc53b1ed0b28d8d0f748ac92
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-14 21:26:00 -0500

    added just one more thing

--- hw/greetings/server.js
+++ hw/greetings/server.js
@@ -4,6 +4,9 @@ app.listen(2023, () => {
     console.log('Hello, Sweetie')
 })
 
+app.get('/greeting', (req, res) => {
+    res.send('<h1>Hello, stranger!</h1>')
+})
 app.get('/greeting/:name', (req, res) => {
     const name = req.params.name
     res.send(`<h1>Hey there, ${name}!</h1>`)
